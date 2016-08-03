71a3978ed3f38628d261c3303f1734fdc56f4993
opendriod <ajaythakur@hotmail.com>
2016-08-02 22:17:55 -0700

    first commit

---
+++ SimpleNotes/AddAttachmentVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddAttachmentVC.swift
+//  SimpleNotes
+//
+//  Created by Ajay Thakur on 7/2/16.
+//  Copyright © 2016 Ajay Thakur. All rights reserved.
+//
+
+import Cocoa
+
+class AddAttachmentVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ SimpleNotes/SimpleNoteCVI.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleNoteCVI.swift
+//  SimpleNotes
+//
+//  Created by Ajay Thakur on 7/2/16.
+//  Copyright © 2016 Ajay Thakur. All rights reserved.
+//
+
+import Cocoa
+
+class SimpleNoteCVI: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
